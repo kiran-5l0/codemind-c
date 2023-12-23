17db0ada06bcb599febbe1849d6b0894cbc5fd8d
kiran ajay <144509002+kiran-5l0@users.noreply.github.com>
2023-12-23 12:50:10 +0530

    #include<stdio.h> int main() {     int m,n,count=0,i;     scanf("%d%d",&m,&n);     for(i=m;i<=n;i++)     {         if(i % 2 == 0 && i % 3 == 0)         {             count++;         }     }     printf("%d",count); }

---
+++ Count_of_numbers_between_M_and_N_which_divisible_by_both_2_and_3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int m,n,count=0,i;
+    scanf("%d%d",&m,&n);
+    for(i=m;i<=n;i++)
+    {
+        if(i % 2 == 0 && i % 3 == 0)
+        {
+            count++;
+        }
+    }
+    printf("%d",count);
+}
